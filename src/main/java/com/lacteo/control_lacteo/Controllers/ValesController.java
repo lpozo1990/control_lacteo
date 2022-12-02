60023510ed9e62a3e4d0756d4d954681c9e280c3
lpozo1990 <lpozo1990@gmail.com>
2022-12-01 21:36:14 -0400

    meses agregados

--- src/main/java/com/lacteo/control_lacteo/Controllers/ValesController.java
+++ src/main/java/com/lacteo/control_lacteo/Controllers/ValesController.java
@@ -1,21 +1,34 @@
 package com.lacteo.control_lacteo.Controllers;
 
 import java.text.DateFormatSymbols;
+import java.util.Arrays;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//function to get slice of a primitive array in Java   
 
 @Controller
 public class ValesController {
-    
+
     @GetMapping("/vales")
     String GetVales(Model model) {
         DateFormatSymbols spanish_dfs = new DateFormatSymbols(Locale.forLanguageTag("es-ES"));
-        String[] months =  spanish_dfs.getMonths();
-        model.addAttribute("meses", months);
+        String[] months = spanish_dfs.getMonths();
+        int startIndex = 0, endIndex = 12;
+        // get the slice of the array
+        String[] sliceArray = slice(months, startIndex, endIndex);
+        System.out.println(Arrays.toString(sliceArray));
+        model.addAttribute("meses", sliceArray);
         return "vales";
+    }
+
+    public static String[] slice(String[] array, int startIndex, int endIndex) {
+        // Get the slice of the Array
+        String[] slicedArray = Arrays.copyOfRange(array, startIndex, endIndex);
+        // return the slice
+        return slicedArray;
     }
 }
