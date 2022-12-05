2bc2cd23f0fb355599d2c9b32a1f863a93d8041d
lpozo1990 <lpozo1990@gmail.com>
2022-12-05 17:20:46 -0400

    Update Choferes

--- src/main/java/com/lacteo/control_lacteo/Controllers/DriversController.java
+++ src/main/java/com/lacteo/control_lacteo/Controllers/DriversController.java
@@ -1,18 +1,21 @@
 package com.lacteo.control_lacteo.Controllers;
 
-import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.google.gson.Gson;
 import com.lacteo.control_lacteo.Entities.Chofer;
 import com.lacteo.control_lacteo.Service.ChoferService;
+import com.lacteo.control_lacteo.repositories.ChoferRepository;
 
 @Controller
 public class DriversController {
@@ -21,6 +24,13 @@ public class DriversController {
     @Autowired
     public void setChoferService(ChoferService choferService) {
         this.choferService = choferService;
+    }
+
+    private ChoferRepository choferRepo;
+
+    @Autowired
+    public void setChoferRepository(ChoferRepository choferRepo) {
+        this.choferRepo = choferRepo;
     }
     @RequestMapping("/")
     String listAll(Model model){
@@ -46,6 +56,12 @@ public class DriversController {
     @RequestMapping(value = "newChofer", method = RequestMethod.POST)
     public String savedriver(Chofer driver) {
         choferService.saveChofer(driver);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "actualizarChofer/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String savedriver(@PathVariable Integer id, @RequestBody MultiValueMap<String, String> formData) {
+        choferRepo.updateChofer(id, formData.getFirst("nombre"), formData.getFirst("apellidos"));
         return "redirect:/";
     }
 }
--- src/main/java/com/lacteo/control_lacteo/repositories/ChoferRepository.java
+++ src/main/java/com/lacteo/control_lacteo/repositories/ChoferRepository.java
@@ -1,6 +1,11 @@
 package com.lacteo.control_lacteo.repositories;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import com.lacteo.control_lacteo.Entities.Chofer;
 
@@ -8,4 +13,12 @@ public interface ChoferRepository extends JpaRepository<Chofer, Integer> {
 
 @Query(value="SELECT nombre, apellidos FROM choferes", nativeQuery = true)
 Object[] findNames();
+
+@Modifying
+@Transactional
+@Query("UPDATE Chofer c SET c.nombre = :nombre, c.apellidos = :apellidos WHERE c.id = :id")
+int updateChofer(@Param("id") int id, @Param("nombre") String nombre, @Param("apellidos") String apellidos);
+
+Optional<Chofer> findById(Integer id);
+
 }
