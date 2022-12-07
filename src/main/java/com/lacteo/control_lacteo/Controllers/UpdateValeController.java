d74a271996d1af898b8b8eab0dd8c5c4a7bb5a05
lpozo1990 <lpozo1990@gmail.com>
2022-12-07 09:44:57 -0400

    Fix

---
+++ src/main/java/com/lacteo/control_lacteo/Controllers/UpdateValeController.java
@@ -0,0 +1,25 @@
+package com.lacteo.control_lacteo.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lacteo.control_lacteo.Entities.Vale;
+import com.lacteo.control_lacteo.repositories.ValeRepository;
+
+@RestController
+public class UpdateValeController {
+
+    private ValeRepository valeRepo;
+
+    @Autowired
+    public void setValeRepository(ValeRepository valeRepo) {
+        this.valeRepo = valeRepo;
+    }
+    @PostMapping("/updateVale")
+    void updateOne(@RequestBody Vale vale){
+        System.out.println(vale);
+        this.valeRepo.updateOne(vale.getCantidad_de_leche(), vale.getNumero_de_vale(), vale.getCodigo_proveedor(), vale.getId());
+    }
+}
--- src/main/java/com/lacteo/control_lacteo/repositories/ValeRepository.java
+++ src/main/java/com/lacteo/control_lacteo/repositories/ValeRepository.java
@@ -2,13 +2,22 @@ package com.lacteo.control_lacteo.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import com.lacteo.control_lacteo.Entities.Vale;
+import org.springframework.stereotype.Repository;
 
+import com.lacteo.control_lacteo.Entities.Vale;
+@Repository
 public interface ValeRepository extends JpaRepository<Vale, Integer> {
     
 @Query(value="SELECT numero_de_vale, codigo_proveedor, cantidad_de_leche  FROM vales LEFT JOIN entradas ON vales.entrada_id = entradas.id WHERE entrada_id=?1", nativeQuery = true)
 List<Object> getValesForId(Integer id);
+
+@Modifying
+@Transactional
+@Query(value="UPDATE vales SET cantidad_de_leche=?1, numero_de_vale=?2 , codigo_proveedor=?3  WHERE entrada_id = ?4", nativeQuery= true)
+void updateOne(Integer cantidad_de_leche, String numero_de_vale ,String codigo_proveedor, Integer id );
 }
